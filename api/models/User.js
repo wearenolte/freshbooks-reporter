5b0e57b84204f196a562e541b1c32576cbce1372
Roberto Dip <dip.jesusr@gmail.com>
2014-09-24 10:44:08 -0300

    Remove some boilerplate that comes with the default sails skeleton

--- api/models/User.js
+++ api/models/User.js
@@ -1,7 +1,7 @@
 /**
  * User.js
  *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @description :: Basic user model
  * @docs        :: http://sailsjs.org/#!documentation/models
  */
 

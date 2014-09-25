969235bb01823fc677b660a46b72db0e78ad6772
Roberto Dip <dip.jesusr@gmail.com>
2014-09-25 11:00:14 -0300

    Add the project model and override the find action to hit the Freshbooks API

---
+++ api/controllers/ProjectController.js
@@ -0,0 +1,19 @@
+/**
+ * ProjectController
+ *
+ * @description :: Server-side logic for managing projects
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+  find: function(req, res) {
+    Freshbooks.api.call("project.list", {}, function(err, response) {
+      if (err) {
+        res.json(500, err);
+      } else {
+        projects = response.response.projects.project;
+        res.json(200, projects);
+      };
+    });
+  }
+};
